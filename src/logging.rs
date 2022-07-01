92874a830acd53d50b57118a6d67a336f659b9b9
AltF2 <contact@altf2.dev>
2022-07-01 19:02:19 +0200

    feature: add logging level via parameter
    Signed-off-by: AltF2 <contact@altf2.dev>

--- src/logging.rs
+++ src/logging.rs
@@ -1,9 +1,9 @@
 use anyhow::Result;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-pub fn setup() -> Result<()> {
+pub fn setup(log: String) -> Result<()> {
     tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(std::env::var("RUST_LOG").unwrap_or_else(|_| "debug".into())))
+        .with(tracing_subscriber::EnvFilter::new(log))
         .with(tracing_subscriber::fmt::layer())
         .init();
 
